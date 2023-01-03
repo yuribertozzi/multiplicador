0bb72fe5bd1d20b3b881784603da3ec74533a24f
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-03 12:29:15 -0300

    Add files via upload

---
+++ multiplicador.py
@@ -0,0 +1,21 @@
+print()
+
+tamanho = int(input("Digite o tamanho da sequência de números: "))
+
+print ()
+
+produto = 1 
+
+i = 0
+
+while i < tamanho:
+	
+	valor = int(input("Digite um valor a ser multiplicado: "))
+	
+	produto = produto * valor
+
+	i = i + 1
+
+print()
+
+print("O produto dos valores digitados é", produto, ".")
